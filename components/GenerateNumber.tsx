7bf4c6a8524d9d922e1c9453e3614b3b688a7d23
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-02-22 22:23:49 -0300

    created function to verify numbers already left

--- components/GenerateNumber.tsx
+++ components/GenerateNumber.tsx
@@ -1,8 +1,9 @@
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 import { dados } from '../data/data';
 import { genarateNumber } from '../utils/generateNumber';
+import { results } from '../types/dataType';
 
 const GenerateNumber = () => {
 
@@ -13,10 +14,37 @@ const GenerateNumber = () => {
     setReady(false);
     const genarate = genarateNumber();
     const findGenarate = dados.filter(value => value.resultados == genarate);
-    console.log(genarate)
-    console.log(findGenarate.length);
 
     setMyNumber(genarate);
+    const newGenarate = genarate.split(" ");
+    let a1 = newGenarate[0];
+    let a2 = newGenarate[1];
+    let a3 = newGenarate[2];
+    let a4 = newGenarate[3];
+    let a5 = newGenarate[4];
+    let a6 = newGenarate[5];
+    // console.log(newGenarate);
+
+    let test;
+    let moreArray: results[] = [] 
+    const testandoArray = dados.map(valeu => {
+      test = valeu.resultados.split(" ");
+      let n = 0;
+      for(let i in test){
+        a1 == test[i] ? n++ : ""
+        a2 == test[i] ? n++ : ""
+        a3 == test[i] ? n++ : ""
+        a4 == test[i] ? n++ : ""
+        a5 == test[i] ? n++ : ""
+        a6 == test[i] ? n++ : ""
+      }
+      if(n>3){
+        moreArray.push(valeu)
+      }
+    });
+
+    console.log("number", genarate)
+    console.log(moreArray);
 
     if(findGenarate.length >= 1) {
       setReady(true);
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 
-import { dados } from '../data/data';
 import GenerateNumber from '../components/GenerateNumber';
 
 const Home: NextPage = () => {
-
 
   return (
     <div className={styles.container}>
