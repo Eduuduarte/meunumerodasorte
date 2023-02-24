4fa991a84d1736099d1a0fdde03467e0075f53ba
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-02-23 23:53:32 -0300

    refatored

---
+++ components/GenerateNumber/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+
+import { dados } from '../../data/data';
+import { genarateNumber } from '../../utils/generateNumber';
+import { verifyNumber } from '../../utils/verifyNumber';
+
+const GenerateNumber = () => {
+
+  const [myNumber, setMyNumber] = useState<string>('');
+  const [ready, setReady ] = useState<boolean>(false);
+
+  const gerarNumeros = () => {
+    setReady(false);
+    const genarate = genarateNumber();
+    const findGenarate = dados.filter(value => value.resultados == genarate);
+
+    const verify = verifyNumber(genarate);
+    setMyNumber(genarate);
+   
+    console.log(verify);
+
+    if(findGenarate.length >= 1) {
+      setReady(true);
+    }
+  }
+  
+  return (
+    <div className={styles.container}>
+      <h1>{myNumber}</h1>
+      {ready && 
+        <h5>Número já sorteado em outro concurso.</h5>
+      }
+      <button onClick={gerarNumeros}>Gerar Números</button>
+    </div>
+  )
+}
+
+export default GenerateNumber;
