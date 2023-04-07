8af720a73961cc6ed18dbc3cca9ebeab106df011
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-04-06 22:20:35 -0300

    update seemsResult

--- components/SeemsResults/index.tsx
+++ components/SeemsResults/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useAppContext } from '../../context/app';
 import styles from './styles.module.css';
+import { TbClover } from 'react-icons/tb'
 
 import NumberSingle from '../NumberSingle';
 import { formatterNumber } from '../../libs/formatter';
@@ -17,6 +18,95 @@ const Index = () => {
                     <div className={styles.titleCard}>
                         <div>Concurso {value.Sorteio} - {formatter.formatterData(value.Data.valueOf())}</div>
                         <span>{value.counts} - Acertos</span>
+                        <span>
+                            {value.counts == 4 &&
+                                <>
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                </>
+                            }
+                            {value.counts == 5 &&
+                                <>
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                </>
+                            }
+                            {value.counts == 6 &&
+                                <>
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                    <TbClover
+                                        width={1}
+                                        height={1}
+                                        color='#2a9d8f'
+                                    />
+                                </>
+                            }
+                        </span>
                     </div>
                     <div className={styles.areaNumber}>
                         {value.resultados.split(" ").map((value, index) => (
